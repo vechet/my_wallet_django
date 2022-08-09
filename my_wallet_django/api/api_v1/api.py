ce8dc86856c473f7f74aaa0523df73925c33335e
vechet <vechet240@gmail.com>
2022-08-09 11:29:55 +0700

    update order fast api

--- my_wallet_django/api/api_v1/api.py
+++ my_wallet_django/api/api_v1/api.py
@@ -6,12 +6,12 @@ api_router.include_router(authentication.router,
                           prefix="/v1", tags=["authentication"])
 api_router.include_router(device.router, prefix="/v1", tags=["device"])
 api_router.include_router(
-    account_type.router, prefix="/v1", tags=["account type"])
-api_router.include_router(
-    currency.router, prefix="/v1", tags=["currency"])
-api_router.include_router(
     category.router, prefix="/v1", tags=["category"])
 api_router.include_router(
     payment_method.router, prefix="/v1", tags=["payment method"])
+api_router.include_router(
+    currency.router, prefix="/v1", tags=["currency"])
+api_router.include_router(
+    account_type.router, prefix="/v1", tags=["account type"])
 api_router.include_router(
     account.router, prefix="/v1", tags=["account"])
