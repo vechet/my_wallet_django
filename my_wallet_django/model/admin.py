44f027978b0eb15de25e12a963c498f39494a64b
vechet <vechet240@gmail.com>
2022-08-14 19:15:05 +0700

    update admin

--- my_wallet_django/model/admin.py
+++ my_wallet_django/model/admin.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 from .models import Account, AccountType, Category, Configuration
 from .models import PhotoAndVideo, Status, ConfigurationParam, Currency
 from .models import Device, ErrorReport, FileDocument, GlobalParam
-# from .models import IncomeOrExpense, NotificationDeviceToken, PaymentMethod
+from .models import IncomeOrExpense, NotificationDeviceToken, PaymentMethod
 
 admin.site.register([Account, AccountType, Category, Configuration])
 admin.site.register([PhotoAndVideo, Status, ConfigurationParam, Currency])
 admin.site.register([Device, ErrorReport, FileDocument, GlobalParam])
-# admin.site.register([IncomeOrExpense, NotificationDeviceToken, PaymentMethod])
+admin.site.register([IncomeOrExpense, NotificationDeviceToken, PaymentMethod])
